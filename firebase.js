48dc331ce4ac77250b413f12d354488e88dd28fc
ryry112002-ux <ryry112002@gmail.com>
2026-02-05 19:13:05 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDP651ZGg2iR_zaixsWcBWgWbUwSVsCeOI",
+    authDomain: "drras-53cc6.firebaseapp.com",
+    projectId: "drras-53cc6",
+    storageBucket: "drras-53cc6.firebasestorage.app",
+    messagingSenderId: "918604278668",
+    appId: "1:918604278668:web:5a3311c85eaa0b06d084a9"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
