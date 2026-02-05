b1140b7434b5a694eb42277f3b7c8184a2ec6fe3
ryry112002-ux <ryry112002@gmail.com>
2026-02-05 19:20:47 +0800

    Update field.js

--- field.js
+++ field.js
@@ -1,1 +1,5 @@
+import { requireRole } from "./guard.js";
+import { logout } from "./auth.js";
+requireRole(["field", "admin"]); // allow admin to test
+document.getElementById("logoutBtn").onclick = logout;
 
